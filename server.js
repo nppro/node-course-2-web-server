990460475d8d335ee00259fb40661b0f26a154c7
Nguyen Phuoc <phamnguyenphuoc@gmail.com>
2017-11-16 08:50:48 +0700

    Tao du an ban dau node js tu hoc theo udemy

---
+++ server.js
@@ -0,0 +1,77 @@
+// import thư viện express
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+// khai báo biến app
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+
+// thực hiện lưu log quá trình request từ client đến server bằng middleware
+app.use((req, res, next) => {
+    var now = new Date().toDateString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (error) => {
+        if(error){
+            console.log('Unable file server.log');
+        }
+    });
+
+    next();
+});
+
+// xử lý bằng middleware để xây dựng trang bảo trì hệ thống
+// nếu có request từ client lên thì render template bảo trì 
+// app.use((req, res, next)=>{
+//     res.render('maintenance.hbs');
+// });
+
+// khai báo biến dùng chung cho các engine template 
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text)=>{
+    return text.toUpperCase();
+})
+
+app.get('/', (req, res)=>{
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        contentPage: 'Wellcome Express Node js by NPPRO',
+    
+    });
+
+    // res.send('<h1>Hello Express</h1>');
+    // res.send({
+    //     name: 'Phuoc',
+    //     like: [
+    //         'games',
+    //         'sing'
+    //     ]
+    // })
+});
+
+app.get('/about',(req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        contentPage: 'Some text here. View content guide for my site'
+    });
+});
+
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res)=>{
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
+});
+
+app.listen(4800, () => {
+    console.log('Server is up on port 4800');
+});
